310a1fdc18f406fa93842da9411a7b7c9bf0bc02
Remi Rampin <remirampin@gmail.com>
2013-06-20 17:10:36 -0400

    PythonSource now sets unconnected output ports
    This is useful when used in controlflow, when the output port is not
    directly connected, but read by name from the module object it got from
    a 'self' port.

--- vistrails/core/modules/basic_modules.py
+++ vistrails/core/modules/basic_modules.py
@@ -813,6 +813,10 @@ class Null(Module):
 ##############################################################################
 
 class CodeRunnerMixin(object):
+    def __init__(self):
+        Module.__init__(self)
+        self.output_ports_order = []
+
     def run_code(self, code_str,
                  use_input=False,
                  use_output=False,
@@ -832,9 +836,8 @@ class CodeRunnerMixin(object):
                               for k in self.inputPorts])
             locals_.update(inputDict)
         if use_output:
-            outputDict = dict([(k, None)
-                               for k in self.outputPorts])
-            locals_.update(outputDict)
+            for output_portname in self.output_ports_order:
+                locals_[output_portname] = None
         _m = core.packagemanager.get_package_manager()
         reg = get_module_registry()
         locals_.update({'fail': fail,
@@ -846,8 +849,8 @@ class CodeRunnerMixin(object):
         del locals_['source']
         exec code_str in locals_, locals_
         if use_output:
-            for k in outputDict.iterkeys():
-                if locals_[k] != None:
+            for k in self.output_ports_order:
+                if locals_.get(k) != None:
                     self.setResult(k, locals_[k])
 
 ##############################################################################
@@ -908,9 +911,8 @@ class SmartSource(NotCacheable, Module):
                               for k in self.inputPorts])
             locals_.update(inputDict)
         if use_output:
-            outputDict = dict([(k, None)
-                               for k in self.outputPorts])
-            locals_.update(outputDict)
+            for output_portname in self.output_ports_order:
+                locals_[output_portname] = None
         _m = core.packagemanager.get_package_manager()
         locals_.update({'fail': fail,
                         'package_manager': _m,
@@ -920,11 +922,11 @@ class SmartSource(NotCacheable, Module):
         exec code_str in locals_, locals_
         if use_output:
             oports = self.registry.get_descriptor(SmartSource).output_ports
-            for k in outputDict.iterkeys():
-                if locals_[k] != None:
+            for k in self.output_ports_order:
+                if locals_.get(k) != None:
                     v = locals_[k]
                     spec = oports.get(k, None)
-                    
+
                     if spec:
                         # See explanation of algo in doc/smart_source_resolution_algo.txt
                         port_vistrail_base_class = spec.types()[0]
